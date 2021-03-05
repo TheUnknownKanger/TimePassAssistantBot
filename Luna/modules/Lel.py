1943ea966c139c8fad1eb2c807294b5a1e2f8be2
Amarnathcdj <72609355+Amarnathcdj@users.noreply.github.com>
2021-03-05 22:12:00 +0530

    Update Lel.py

--- Luna/modules/Lel.py
+++ Luna/modules/Lel.py
@@ -1,13 +1,13 @@
 from Luna import SUDO_USERS as k
 from Luna.events import register
-@register(pattern="^/frwd")
+@register(pattern="^/s")
 async def _(event):
    p = list(k)
    reply = "**Known Dragon Disasters 🔥:**\n"
    for m in p:
         user_id = int(m)
         try:
-            reply += f"• {m}\n"
+            reply += f"• [{m}](tg://user?id={m})\n"
         except Exception:
             pass
    await event.reply(reply)
