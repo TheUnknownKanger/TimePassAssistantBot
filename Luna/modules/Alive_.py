e78b752c6276d9de297c8e7a842ecfd34f2c120c
Amarnathcdj <72609355+Amarnathcdj@users.noreply.github.com>
2021-03-06 21:55:46 +0530

    Update Alive_.py

--- Luna/modules/Alive_.py
+++ Luna/modules/Alive_.py
@@ -1,12 +1,13 @@
 from Luna import tbot, OWNER_ID, StartTime
-
+import time
 from Luna.events import register
 
 @register(pattern="^/alive")
 async def _(event):
+    uptime = get_readable_time((time.time() - StartTime))
     ok = event.chat.title
     reply = "**I'm Up And Alive**\n\n"
-    reply += f"**Awake Since:** {StartTime}\n"
+    reply += f"**Awake Since: {uptime}**\n"
     reply += "**Telethon Ver: 1.20.0**\n"
     reply += "**Bot_Ver: 1.8**\n"
     reply += f"**Chat: {ok}**"
