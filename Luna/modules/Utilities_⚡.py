eccd70be599ed539ddbebb2177e0ca7fd73bb0bd
Amarnathcdj <72609355+Amarnathcdj@users.noreply.github.com>
2021-03-06 22:06:02 +0530

    Update Utilities_⚡.py

--- Luna/modules/Utilities_⚡.py
+++ Luna/modules/Utilities_⚡.py
@@ -411,10 +411,10 @@ async def _(event):
     uptime = get_readable_time((time.time() - StartTime))
     ok = event.chat.title
     reply = "**I'm Up And Alive**\n\n"
-    reply += f"**Awake Since: {uptime}**\n"
-    reply += "**Telethon Ver: 1.20.0**\n"
-    reply += "**Bot_Ver: 1.8**\n"
-    reply += f"**Chat: {ok}**"
+    reply += f"**Awake Since:** {uptime}\n"
+    reply += "**Telethon Ver:** 1.20.0\n"
+    reply += "**Bot_Ver:** 1.8\n"
+    reply += f"**Chat:** {ok}"
     await event.reply(reply)
 
 @register(pattern="^/staffs")
