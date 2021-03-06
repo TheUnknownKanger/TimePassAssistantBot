dd066e940053b680881a3e60a42f99d44e1e8c24
Amarnathcdj <72609355+Amarnathcdj@users.noreply.github.com>
2021-03-06 20:29:35 +0530

    Update Alive_.py

--- Luna/modules/Alive_.py
+++ Luna/modules/Alive_.py
@@ -4,7 +4,11 @@ from Luna.events import register
 
 @register(pattern="^/alive")
 async def _(event):
+    k = await tbot.get_entity(OWNER_ID)
+    fname = k.first_name
+    lname = k.last_name
     reply = "**I'm Alive Master**\n\n"
     reply += f"**Awake Since:** {StartTime}"
+    reply += f"**Owner:** {fname}-{lname}"
     await event.reply(reply)
     
