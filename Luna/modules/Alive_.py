a236851369ca0076f04a893141c9e37672f3c184
Amarnathcdj <72609355+Amarnathcdj@users.noreply.github.com>
2021-03-06 20:40:44 +0530

    Update Alive_.py

--- Luna/modules/Alive_.py
+++ Luna/modules/Alive_.py
@@ -4,9 +4,11 @@ from Luna.events import register
 
 @register(pattern="^/alive")
 async def _(event):
+    ok = event.chat.title
     reply = "**I'm Up And Alive**\n\n"
     reply += f"**Awake Since:** {StartTime}\n"
     reply += "**Telethon Ver: 1.20.0**\n"
     reply += "**Bot_Ver: 1.8**"
+    reply += f"**Chat: {ok}"
     await event.reply(reply)
     
