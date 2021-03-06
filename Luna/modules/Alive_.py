ebee9f797e900348f014191ba2f2c5db1e2a2067
Amarnathcdj <72609355+Amarnathcdj@users.noreply.github.com>
2021-03-06 20:39:29 +0530

    Update Alive_.py

--- Luna/modules/Alive_.py
+++ Luna/modules/Alive_.py
@@ -5,7 +5,7 @@ from Luna.events import register
 @register(pattern="^/alive")
 async def _(event):
     reply = "**I'm Up And Alive**\n\n"
-    reply += f"**Awake Since:** {StartTime}"
+    reply += f"**Awake Since:** {StartTime}\n"
     reply += "**Telethon Ver: 1.20.0**\n"
     reply += "**Bot_Ver: 1.8**"
     await event.reply(reply)
