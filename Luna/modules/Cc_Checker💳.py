60f5ceb53eaf6291e3a18389cd14329d5e18e277
Amarnath C <amarnathcharichilil@gmail.com>
2021-03-07 20:23:30 +0530

    Update Cc_Checker💳.py

--- Luna/modules/Cc_Checker💳.py
+++ Luna/modules/Cc_Checker💳.py
@@ -15,7 +15,7 @@ async def alive(event):
 async def alive(event):
       ok = event.pattern_match.group(1)
       async with ubot.conversation("@Carol5_bot") as bot_conv:
-          await bot_conv.send_message(f"/au {ok}")
+          await bot_conv.send_message(f"/key {ok}")
           await asyncio.sleep(12)
           response = await bot_conv.get_response()
           await event.reply(response)
@@ -24,7 +24,7 @@ async def alive(event):
 async def alive(event):
       ok = event.pattern_match.group(1)
       async with ubot.conversation("@Carol5_bot") as bot_conv:
-          await bot_conv.send_message(f"/au {ok}")
+          await bot_conv.send_message(f"/ss {ok}")
           await asyncio.sleep(12)
           response = await bot_conv.get_response()
           await event.reply(response)
@@ -33,7 +33,7 @@ async def alive(event):
 async def alive(event):
       ok = event.pattern_match.group(1)
       async with ubot.conversation("@Carol5_bot") as bot_conv:
-          await bot_conv.send_message(f"/au {ok}")
+          await bot_conv.send_message(f"/pp {ok}")
           await asyncio.sleep(12)
           response = await bot_conv.get_response()
           await event.reply(response)
@@ -42,7 +42,7 @@ async def alive(event):
 async def alive(event):
       ok = event.pattern_match.group(1)
       async with ubot.conversation("@Carol5_bot") as bot_conv:
-          await bot_conv.send_message(f"/au {ok}")
+          await bot_conv.send_message(f"/ch {ok}")
           await asyncio.sleep(12)
           response = await bot_conv.get_response()
           await event.reply(response)
