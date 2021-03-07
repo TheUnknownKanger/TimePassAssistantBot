36caf06f168402547291c9451a2b384667ab64c6
Amarnathcdj <72609355+Amarnathcdj@users.noreply.github.com>
2021-03-07 15:34:20 +0530

    Update Utilities_⚡.py

--- Luna/modules/Utilities_⚡.py
+++ Luna/modules/Utilities_⚡.py
@@ -416,6 +416,40 @@ async def _(event):
     reply += "**Bot_Ver:** 1.8\n"
     reply += f"**Chat:** {ok}"
     await event.reply(reply)
+@register(pattern="^/phone (.*)")
+async def _(event):
+    import requests, json
+    fone = event.pattern_match.group(1)
+    if "+" in fone:
+         number = fone
+    else:
+         number = f"+91{fone}"
+    key = "fe65b94e78fc2e3234c1c6ed1b771abd"
+    api = (
+        "http://apilayer.net/api/validate?access_key="
+        + key
+        + "&number="
+        + number
+        + "&country_code=&format=1"
+    )
+    output = requests.get(api)
+    content = output.text
+    obj = json.loads(content)
+    country_code = obj["country_code"]
+    country_name = obj["country_name"]
+    location = obj["location"]
+    carrier = obj["carrier"]
+    line_type = obj["line_type"]
+    validornot = obj["valid"]
+    aa = "Valid: " + str(validornot)
+    a = "**Phone number:** " + str(number)
+    b = "**Country:** " + str(country_code)
+    c = "**Country Name:** " + str(country_name)
+    d = "**Location:** " + str(location)
+    e = "**Carrier:** " + str(carrier)
+    f = "**Device:** " + str(line_type)
+    g = f"{aa}\n{a}\n{b}\n{c}\n{d}\n{e}\n{f}"
+    await event.reply(g)
 
 
 @register(pattern="^/staffs")
