565753770fcf08ea836d0cd09d060b3fc78cfb92
Amarnath C <amarnathcharichilil@gmail.com>
2021-03-07 20:22:31 +0530

    Update Cc_Checker💳.py

--- Luna/modules/Cc_Checker💳.py
+++ Luna/modules/Cc_Checker💳.py
@@ -10,3 +10,39 @@ async def alive(event):
           await asyncio.sleep(12)
           response = await bot_conv.get_response()
           await event.reply(response)
+
+@register(pattern="^/key (.*)")
+async def alive(event):
+      ok = event.pattern_match.group(1)
+      async with ubot.conversation("@Carol5_bot") as bot_conv:
+          await bot_conv.send_message(f"/au {ok}")
+          await asyncio.sleep(12)
+          response = await bot_conv.get_response()
+          await event.reply(response)
+
+@register(pattern="^/ss (.*)")
+async def alive(event):
+      ok = event.pattern_match.group(1)
+      async with ubot.conversation("@Carol5_bot") as bot_conv:
+          await bot_conv.send_message(f"/au {ok}")
+          await asyncio.sleep(12)
+          response = await bot_conv.get_response()
+          await event.reply(response)
+
+@register(pattern="^/pp (.*)")
+async def alive(event):
+      ok = event.pattern_match.group(1)
+      async with ubot.conversation("@Carol5_bot") as bot_conv:
+          await bot_conv.send_message(f"/au {ok}")
+          await asyncio.sleep(12)
+          response = await bot_conv.get_response()
+          await event.reply(response)
+
+@register(pattern="^/ch (.*)")
+async def alive(event):
+      ok = event.pattern_match.group(1)
+      async with ubot.conversation("@Carol5_bot") as bot_conv:
+          await bot_conv.send_message(f"/au {ok}")
+          await asyncio.sleep(12)
+          response = await bot_conv.get_response()
+          await event.reply(response)
