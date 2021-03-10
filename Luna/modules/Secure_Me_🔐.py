5d83a5a8f963e8310683804e4152da32c329305e
Amarnath C <amarnathcharichilil@gmail.com>
2021-03-10 14:43:53 +0530

    Update Secure_Me_🔐.py

--- Luna/modules/Secure_Me_🔐.py
+++ Luna/modules/Secure_Me_🔐.py
@@ -1,6 +1,8 @@
-from Luna import tbot
+from Luna import tbot, CMD_HELP
+import os
 from Luna.events import register
 import secureme
+
 @register(pattern="^/encrypt ?(.*)")
 async def hmm(event):
     if event.reply_to_msg_id:
@@ -22,3 +24,14 @@ async def hmm(event):
     Text = ok
     k = secureme.decrypt(Text)
     await event.reply(k)
+
+file_help = os.path.basename(__file__)
+file_help = file_help.replace(".py", "")
+file_helpo = file_help.replace("_", " ")
+
+__help__ = """
+ - /encrypt: Encrypts The Given Text
+ - /decrypt: Decrypts Previously Ecrypted Text
+"""
+
+CMD_HELP.update({file_helpo: [file_helpo, __help__]})
