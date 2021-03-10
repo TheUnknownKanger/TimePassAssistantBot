669e202f7a93e6bfa76db8fe92504a1cf6b1e973
Amarnath C <amarnathcharichilil@gmail.com>
2021-03-10 22:25:02 +0530

    Update Youtube_📀.py

--- Luna/modules/Youtube_📀.py
+++ Luna/modules/Youtube_📀.py
@@ -57,3 +57,14 @@ async def inline_id_handler(event: events.InlineQuery.Event):
                 )
             )
         await event.answer(results)
+
+
+file_help = os.path.basename(__file__)
+file_help = file_help.replace(".py", "")
+file_helpo = file_help.replace("_", " ")
+
+__help__ = """
+ - @AnieGrpBot <query>: YouTube Inline Search
+"""
+
+CMD_HELP.update({file_helpo: [file_helpo, __help__]})
