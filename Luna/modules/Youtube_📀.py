38a2bb06190ed7da42286ad774f99904330d218f
Amarnath C <amarnathcharichilil@gmail.com>
2021-03-10 21:52:06 +0530

    Create Youtube_📀.py

---
+++ Luna/modules/Youtube_📀.py
@@ -0,0 +1,54 @@
+from Luna import tbot
+
+import os
+import re
+import urllib
+from math import ceil
+
+import requests
+from telethon import Button, custom, events, functions
+from youtubesearchpython import SearchVideos
+
+@tbot.on(events.InlineQuery(pattern=r"yt (.*)"))
+async def inline_id_handler(event: events.InlineQuery.Event):
+    builder = event.builder
+    testinput,legendx = event.pattern_match.group(1).split(";")
+    urllib.parse.quote_plus(testinput)
+    lund = event.sender_id
+    if lund == lund:
+        results = []
+        search = SearchVideos(f"{testinput}", offset=1, mode="dict", max_results=int(legendx))
+        mi = search.result()
+        moi = mi["search_result"]
+        if search == None:
+            resultm = builder.article(
+                title="No Results.",
+                description="Try Again With correct Spelling",
+                text="**No Matching Found**",
+                buttons=[
+                    [Button.switch_inline("Search Again", query="yt ", same_peer=True)],
+                ],
+            )
+            await event.answer([resultm])
+            return
+        for mio in moi:
+            mo = mio["link"]
+            thum = mio["title"]
+            proboyx = mio["id"]
+            thums = mio["channel"]
+            td = mio["duration"]
+            tw = mio["views"]
+            kekme = f"https://img.youtube.com/vi/{proboyx}/hqdefault.jpg"
+            okayz = f"**Title :** `{thum}` \n**Link :** {mo} \n**Channel :** `{thums}` \n**Views :** `{tw}` \n**Duration :** `{td}`"
+            hmmkek = f"Channel : {thums} \nDuration : {td} \nViews : {tw}"
+            results.append(
+                await event.builder.article(
+                    title=thum,
+                    description=hmmkek,
+                    text=okayz,
+                    buttons=Button.switch_inline(
+                        "Search Again", query="yt ", same_peer=True
+                    ),
+                )
+            )
+        await event.answer(results)
