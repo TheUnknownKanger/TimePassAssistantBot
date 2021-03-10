53d9ed57d4fa55c14b276d537f9394aaad2cb624
Amarnath C <amarnathcharichilil@gmail.com>
2021-03-10 19:51:48 +0530

    Create RMBG_✏️.py

---
+++ Luna/modules/RMBG_✏️.py
@@ -0,0 +1,108 @@
+from Luna import CMD_HELP
+from Luna import tbot
+import io
+import asyncio
+import os
+from datetime import datetime
+
+import requests
+from telethon import types
+from telethon.tl import functions
+from Luna import REM_BG_API_KEY
+from Luna import TEMP_DOWNLOAD_DIRECTORY
+from Luna.events import register
+
+
+@register(pattern="^/rmbg")
+async def _(event):
+    HELP_STR = "use `/rmbg` as reply to a media"
+    if event.fwd_from:
+        return
+    if REM_BG_API_KEY is None:
+        await event.reply("You need API token from remove.bg to use this plugin.")
+        return False
+    start = datetime.now()
+    message_id = event.message.id
+    if event.reply_to_msg_id:
+        message_id = event.reply_to_msg_id
+        reply_message = await event.get_reply_message()
+        s = await event.reply("Processing...")
+        try:
+            downloaded_file_name = await tbot.download_media(
+                reply_message, TEMP_DOWNLOAD_DIRECTORY
+            )
+        except Exception as e:
+            await event.reply(str(e))
+            return
+        else:
+            output_file_name = ReTrieveFile(downloaded_file_name)
+            os.remove(downloaded_file_name)
+    else:
+        await event.reply(HELP_STR)
+        return
+    contentType = output_file_name.headers.get("content-type")
+    if "image" in contentType:
+        with io.BytesIO(output_file_name.content) as remove_bg_image:
+            remove_bg_image.name = "rmbg.png"
+            await tbot.send_file(
+                event.chat_id,
+                remove_bg_image,
+                force_document=True,
+                supports_streaming=False,
+                allow_cache=False,
+                reply_to=message_id,
+            )
+        end = datetime.now()
+        ms = (end - start).seconds
+        await s.edit("Background Removed in {} seconds".format(ms))
+        await asyncio.sleep(2)
+        await s.delete()
+    else:
+        d = await event.reply(
+            "remove.bg API returned Errors. Please report to @lunabotsupport\n`{}".format(
+                output_file_name.content.decode("UTF-8")
+            )
+        )
+        await asyncio.sleep(6)
+        await d.delete()
+        
+
+
+def ReTrieveFile(input_file_name):
+    headers = {
+        "X-API-Key": REM_BG_API_KEY,
+    }
+    files = {
+        "image_file": (input_file_name, open(input_file_name, "rb")),
+    }
+    r = requests.post(
+        "https://api.remove.bg/v1.0/removebg",
+        headers=headers,
+        files=files,
+        allow_redirects=True,
+        stream=True,
+    )
+    return r
+@register(pattern="^/superfban")
+async def _(event):
+    if event.reply_to_msg_id:
+        k = await event.reply("Initiating SuperFedban..")
+        await asyncio.sleep(4)
+        await k.edit("Banned User Successfully In 322 Feds")
+    else:
+        await event.reply("Abe Kisko Krna He Bsd")
+        return
+file_help = os.path.basename(__file__)
+file_help = file_help.replace(".py", "")
+file_helpo = file_help.replace("_", " ")
+
+__help__ = """
+ - /rmbg: Type in reply to a media to remove it's background
+"""
+
+CMD_HELP.update({
+    file_helpo: [
+        file_helpo,
+        __help__
+    ]
+})
