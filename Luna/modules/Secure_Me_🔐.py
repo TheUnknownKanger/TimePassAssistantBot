7e550a20ecf7ef35019fdac03073c6cd4a0a4d19
Amarnath C <amarnathcharichilil@gmail.com>
2021-03-10 10:22:50 +0530

    Update Secure_Me_🔐.py

--- Luna/modules/Secure_Me_🔐.py
+++ Luna/modules/Secure_Me_🔐.py
@@ -3,14 +3,20 @@ from Luna.events import register
 import secureme
 @register(pattern="^/encrypt (.*)")
 async def hmm(event):
-    cmd = event.pattern_match.group(1)
+    if event.reply_to_msg_id:
+          cmd = await event.get_reply_message()
+    else:
+          cmd = event.pattern_match.group(1)
     Text = cmd
     k = secureme.encrypt(Text)
     await event.reply(k)
 
 @register(pattern="^/decrypt (.*)")
 async def hmm(event):
-    ok = event.pattern_match.group(1)
+    if event.reply_to_msg_id:
+          ok = await event.get_reply_message()
+    else:
+          ok = event.pattern_match.group(1)
     Text = ok
     k = secureme.decrypt(Text)
     await event.reply(k)
