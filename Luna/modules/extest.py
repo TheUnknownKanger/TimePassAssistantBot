4b3222dbbe22c3e76f7e29e6aba63907192d7c06
Amarnath C <amarnathcharichilil@gmail.com>
2021-03-10 17:59:21 +0530

    Update extest.py

--- Luna/modules/extest.py
+++ Luna/modules/extest.py
@@ -54,3 +54,24 @@ async def disable_ws(event):
     rmnightmode(str(event.chat_id))
     await event.reply(f"**Removed Chat {event.chat.title} With Id {event.chat_id} From Database. This Group Will Be No Longer Closed On 12Am(IST) And Will Opened On 06Am(IST)**")
 
+
+async def job_close():
+    ws_chats = get_all_chat_id()
+    if len(ws_chats) == 0:
+        return
+    for warner in ws_chats:
+        try:
+            await tbot.send_message(
+              int(warner.chat_id), "`12:00 Am, Group Is Closing Till 6 Am. Night Mode Started !` \n**Powered By @FRidayOT**"
+            )
+            await tbot(
+            functions.messages.EditChatDefaultBannedRightsRequest(
+                peer=int(warner.chat_id), banned_rights=hehes
+            )
+            )
+        except Exception as e:
+            logger.info(f"Unable To Close Group {warner} - {e}")
+
+scheduler = AsyncIOScheduler(timezone="Asia/Kolkata")
+scheduler.add_job(job_close, trigger="cron", hour=23, minute=55)
+scheduler.start()
