d8287e81ebc5a4dc24f03e84f20f57fe40b468c3
Amarnath C <amarnathcharichilil@gmail.com>
2021-03-10 22:30:13 +0530

    Update Youtube_🎬.py

--- Luna/modules/Youtube_🎬.py
+++ Luna/modules/Youtube_🎬.py
@@ -67,7 +67,6 @@ __help__ = """
  - @AnieGrpBot <query>: YouTube Inline Search
 
 **Note:** Optional, add ; after query to specify Number of Results
-Example Syntax: @Aniegrpbot Dude perfect;3
 """
 
 CMD_HELP.update({file_helpo: [file_helpo, __help__]})
