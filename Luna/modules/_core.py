96fc6271af166221bef108ef4973bbdf1239c263
Amarnath C <amarnathcharichilil@gmail.com>
2021-03-10 13:12:22 +0530

    Create _core.py

---
+++ Luna/modules/_core.py
@@ -0,0 +1,101 @@
+from Luna import tbot, OWNER_ID
+from Luna.events import register
+import os
+import asyncio
+import os
+import time
+from datetime import datetime
+from Luna import TEMP_DOWNLOAD_DIRECTORY as path
+from Luna import TEMP_DOWNLOAD_DIRECTORY
+from datetime import datetime
+water = './Luna/resources/2326fe2326f8f362760c71bb60d2fee5.jpg'
+client = tbot
+
+@register(pattern=r"^/send ?(.*)")
+async def Prof(event):
+    if event.sender_id == OWNER_ID:
+        pass
+    else:
+        return
+    thumb = water
+    message_id = event.message.id
+    input_str = event.pattern_match.group(1)
+    the_plugin_file = "./Luna/modules/{}.py".format(input_str)
+    if os.path.exists(the_plugin_file):
+     message_id = event.message.id
+     await event.client.send_file(
+             event.chat_id,
+             the_plugin_file,
+             force_document=True,
+             allow_cache=False,
+             thumb=thumb,
+             reply_to=message_id,
+         )
+    else:
+        await event.reply("No File Found!")
+
+import time
+from io import BytesIO
+from pathlib import Path
+from Luna import tbot as borg
+from telethon import functions, types
+from telethon.errors import PhotoInvalidDimensionsError
+from telethon.errors.rpcerrorlist import YouBlockedUserError
+from telethon.tl.functions.messages import SendMediaRequest
+@register(pattern="^/ttf ?(.*)")
+async def get(event):
+    name = event.text[5:]
+    if name is None:
+        await event.reply("reply to text message as `.ttf <file name>`")
+        return
+    m = await event.get_reply_message()
+    if m.text:
+        with open(name, "w") as f:
+            f.write(m.message)
+        await event.delete()
+        await event.client.send_file(event.chat_id, name, force_document=True)
+        os.remove(name)
+    else:
+        await event.reply("reply to text message as `.ttf <file name>`")
+
+from Luna.events import load_module
+import asyncio
+import os
+from datetime import datetime
+from pathlib import Path
+
+@register(pattern="^/install")
+async def install(event):
+    if event.fwd_from:
+        return
+    if event.sender_id == OWNER_ID:
+        pass
+    else:
+        return
+    if event.reply_to_msg_id:
+        try:
+            downloaded_file_name = (
+                await event.client.download_media(  # pylint:disable=E0602
+                    await event.get_reply_message(),
+                    "Luna/modules/",  # pylint:disable=E0602
+                )
+            )
+            if "(" not in downloaded_file_name:
+                path1 = Path(downloaded_file_name)
+                shortname = path1.stem
+                load_module(shortname.replace(".py", ""))
+                await event.reply("Installed.... Ab Full Masti💥\n `{}`".format(
+                        os.path.basename(downloaded_file_name)
+                    ),
+                )
+            else:
+                os.remove(downloaded_file_name)
+                await event.reply("**Error!**\nCannot Install!\n Or Pre Installed Maybe..",
+                )
+        except Exception as e:  # pylint:disable=C0103,W0703
+            j = await event.reply(str(e))
+            await asyncio.sleep(3)
+            await j.delete()
+            os.remove(downloaded_file_name)
+    await asyncio.sleep(3)
+    await event.delete()
