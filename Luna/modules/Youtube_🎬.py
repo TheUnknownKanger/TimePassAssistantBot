b5a5a26212811e60f12bb27fbeaa07e70032a9bc
Amarnath C <amarnathcharichilil@gmail.com>
2021-03-11 13:18:24 +0530

    Update Youtube_🎬.py

--- Luna/modules/Youtube_🎬.py
+++ Luna/modules/Youtube_🎬.py
@@ -1,6 +1,6 @@
-
+import os
 from Luna.events import register
-from Luna import tbot, OWNER_ID
+from Luna import tbot, OWNER_ID, CMD_HELP
 from time import sleep
 client = tbot
 chat = -1001309757591
@@ -20,3 +20,16 @@ async def sleepybot(time):
     else:
        await time.reply(f"I am sulking and snoozing....for 10'secs")
        sleep(10)
+
+
+file_help = os.path.basename(__file__)
+file_help = file_help.replace(".py", "")
+file_helpo = file_help.replace("_", " ")
+
+__help__ = """
+ - @AniegrpBot yt <query>: Inline YouTube Search.
+
+**Note:** Optional, Add ; after Query to Specify No. Of Results.
+"""
+
+CMD_HELP.update({file_helpo: [file_helpo, __help__]})
