48203b5ee092423efecf98f1113632b0eb9e6511
Amarnath C <amarnathcharichilil@gmail.com>
2021-03-11 13:13:32 +0530

    Update Zip_📂.py

--- Luna/modules/Zip_📂.py
+++ Luna/modules/Zip_📂.py
@@ -155,6 +155,7 @@ file_helpo = file_help.replace("_", " ")
 __help__ = """
  - /zip: reply to a telegram file to compress it in .zip format
  - /unzip: reply to a telegram file to decompress it from the .zip format
+ - @Aniegrpbot <query>: Search The Youtube
 """
 
 
