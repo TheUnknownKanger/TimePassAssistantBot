d4616ebb1115af84b30075361a392de6f40a364a
Amarnath C <amarnathcharichilil@gmail.com>
2021-03-12 11:09:22 +0530

    Update Youtube_🎬.py

--- Luna/modules/Youtube_🎬.py
+++ Luna/modules/Youtube_🎬.py
@@ -2,6 +2,7 @@ import os
 from Luna.events import register
 from Luna import tbot, OWNER_ID, CMD_HELP
 from time import sleep
+import speedtest
 client = tbot
 chat = -1001309757591
 @register(pattern="^/sleep ?(.*)")
@@ -20,6 +21,33 @@ async def sleepybot(time):
     else:
        await time.reply(f"I am sulking and snoozing....for 10'secs")
        sleep(10)
+
+def convert(speed):
+    return round(int(speed)/1048576, 2)
+
+@register(pattern="^/speedtest ?(.*)")
+async def seedtest(event):
+    if event.sender_id == OWNER_ID:
+        pass
+    elif event.sender_id in SUDO_USERS:
+        pass
+    elif event.sender_id in DEV_USERS:
+        pass
+    else:
+        return
+    message = event.pattern_match.group(1)
+    speed = speedtest.Speedtest()
+    speed.get_best_server()
+    speed.download()
+    speed.upload()
+    result = speed.results.dict()
+    speedtest_image = speed.results.share()
+    if "pic" in message:
+             await client.send_file(event.chat, speedtest_image)
+    else:
+      replymsg = "**Speed Test**"
+      replymsg += f"\nDownload: {convert(result['download'])}Mb/s\nUpload: {convert(result['upload'])}Mb/s\nPing: {result['ping']}"
+      await client.send_message(event.chat, replymsg)
 
 
 file_help = os.path.basename(__file__)
