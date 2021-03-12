ba87a1edd94e3669bbe87a07834966d2b1bdbe87
Amarnath C <amarnathcharichilil@gmail.com>
2021-03-12 19:33:26 +0530

    Update __init__.py

--- Luna/__init__.py
+++ Luna/__init__.py
@@ -98,11 +98,10 @@ if ENV:
         sys.exit(1)
     try:
         DRAGONS = set(int(x) for x in os.environ.get("DRAGONS", "").split())
-        DEV_USERS = set(int(x) for x in os.environ.get("DEV_USERS", "").split())
     except ValueError:
         raise Exception("Your sudo or dev users list does not contain valid integers.")
 
-    DRAGONS = list(DRAGONS) + list(DEV_USERS)
+    DRAGONS = list(DRAGONS)
 
 else:
     sys.exit(1)
