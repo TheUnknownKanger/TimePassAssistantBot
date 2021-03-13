d47178bc9b3ef638d3a0329be3774ddd9afd342f
Amarnath C <amarnathcharichilil@gmail.com>
2021-03-13 22:39:46 +0530

    Update Misc.py

--- Luna/modules/Misc.py
+++ Luna/modules/Misc.py
@@ -1,1 +1,39 @@
+from Luna import tbot, CMD_HELP
+import os
+from Luna.events import register
+import secureme
 
+@register(pattern="^/encrypt ?(.*)")
+async def hmm(event):
+    if event.reply_to_msg_id:
+          lel = await event.get_reply_message()
+          cmd = lel.text
+    else:
+          cmd = event.pattern_match.group(1)
+    Text = cmd
+    k = secureme.encrypt(Text)
+    await event.reply(k)
+
+@register(pattern="^/decrypt ?(.*)")
+async def hmm(event):
+    if event.reply_to_msg_id:
+          lel = await event.get_reply_message()
+          ok = lel.text
+    else:
+          ok = event.pattern_match.group(1)
+    Text = ok
+    k = secureme.decrypt(Text)
+    await event.reply(k)
+
+file_help = os.path.basename(__file__)
+file_help = file_help.replace(".py", "")
+file_helpo = file_help.replace("_", " ")
+
+__help__ = """
+ - /encrypt: Encrypts The Given Text
+ - /decrypt: Decrypts Previously Ecrypted Text
+
+**Note:** Encrypted Using 69 Bit RSA Encryption
+"""
+
+CMD_HELP.update({file_helpo: [file_helpo, __help__]})
