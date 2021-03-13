07c4cd68e3b416b499316c56741d97bd8b1e7058
Amarnath C <amarnathcharichilil@gmail.com>
2021-03-13 16:11:22 +0530

    Create  NSFW_Watch_🐙.py

---
+++ Luna/modules/NSFW_Watch_🐙.py
@@ -0,0 +1,29 @@
+from Luna import CMD_HELP, OWNER_ID
+from Luna.events import register
+import os
+
+k = 'News Py Files In Another Repo'
+@register(pattern="^/teg ?(.*)")
+async def sleepybot(time):
+    if time.fwd_from:
+        return
+    if time.sender_id == OWNER_ID:
+        pass
+    else:
+        return
+    await time.reply("AntiSpam Py In App 2")
+
+file_help = os.path.basename(__file__)
+file_help = file_help.replace(".py", "")
+file_helpo = file_help.replace("_", " ")
+
+__help__ = """
+ - /addnsfw: Adds The Group to nsfw Watch List
+ - /rmnsfw: Removes The Group From nsfw Watch List
+
+**Use:** Nsfw Contents in group gets deleted and logged.
+"""
+
+CMD_HELP.update({file_helpo: [file_helpo, __help__]})
+
+
