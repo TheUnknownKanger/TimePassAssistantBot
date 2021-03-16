b311a7429b475c35fe21cf7a422be57bba64a468
Amarnath C <amarnathcharichilil@gmail.com>
2021-03-16 21:20:44 +0530

    Update Logo_🦇.py

--- Luna/modules/Logo_🦇.py
+++ Luna/modules/Logo_🦇.py
@@ -24,9 +24,9 @@ async def lego(event):
  d = ImageDraw.Draw(txt)
 
 # draw text, half opacity
- d.text((500,300), text, font=fnt, fill=(255,255,255,128))
+ d.text((500,-100), text, font=fnt, fill=(255,255,255,128))
 # draw text, full opacity
- d.text((400,250), text, font=fnt, fill=(255,255,255,255))
+ d.text((400,100), text, font=fnt, fill=(255,255,255,255))
 
  out = Image.alpha_composite(base, txt)
  fname = 'lel.png'
