5c2528c531f1da010a19497a89c0c05def4475b1
Amarnath C <amarnathcharichilil@gmail.com>
2021-03-16 12:29:05 +0530

    Update _exec.py

--- Luna/modules/_exec.py
+++ Luna/modules/_exec.py
@@ -1,7 +1,7 @@
 import subprocess
 from Luna import tbot
-from Luna.events import register
-from Luna import OWNER_ID, SUDO_USERS, DEV_USERS
+from Luna.events import register, lunabot
+from Luna import OWNER_ID, SUDO_USERS, DEV_USERS, abot as ubot
 import asyncio
 import traceback
 import io
@@ -130,3 +130,60 @@ async def aexec(code, smessatatus):
         + "".join(f"\n {l}" for l in code.split("\n"))
     )
     return await locals()["__aexec"](message, reply, tbot, p)
+
+
+
+@lunabot(pattern=".eval")
+async def _(event):
+    check = event.message.sender_id
+    checkint = int(check)
+    # print(checkint)
+    if int(check) != int(OWNER_ID):
+        return
+    cmd = event.text.split(" ", maxsplit=1)[1]
+    reply_to_id = event.message.id
+    if event.reply_to_msg_id:
+        reply_to_id = event.reply_to_msg_id
+
+    old_stderr = sys.stderr
+    old_stdout = sys.stdout
+    redirected_output = sys.stdout = io.StringIO()
+    redirected_error = sys.stderr = io.StringIO()
+    stdout, stderr, exc = None, None, None
+
+    try:
+        await aexec(cmd, event)
+    except Exception:
+        exc = traceback.format_exc()
+
+    stdout = redirected_output.getvalue()
+    stderr = redirected_error.getvalue()
+    sys.stdout = old_stdout
+    sys.stderr = old_stderr
+
+    evaluation = ""
+    if exc:
+        evaluation = exc
+    elif stderr:
+        evaluation = stderr
+    elif stdout:
+        evaluation = stdout
+    else:
+        evaluation = "Success 😃"
+
+    final_output = "**OUTPUT**:\n\n`{}`".format(evaluation)
+    MAX_MESSAGE_SIZE_LIMIT = 4095
+    if len(final_output) > MAX_MESSAGE_SIZE_LIMIT:
+        with io.BytesIO(str.encode(final_output)) as out_file:
+            out_file.name = "eval.text"
+            await ubot.send_file(
+                event.chat_id,
+                out_file,
+                force_document=True,
+                allow_cache=False,
+                caption=cmd,
+                reply_to=reply_to_id,
+            )
+
+    else:
+        await event.reply(final_output)
