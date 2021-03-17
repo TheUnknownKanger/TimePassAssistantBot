23779b99c6b1307ef88587366f7d95c44c5b33ee
Amarnath C <amarnathcharichilil@gmail.com>
2021-03-17 11:50:49 +0530

    Update Logo_💥.py

--- Luna/modules/Logo_💥.py
+++ Luna/modules/Logo_💥.py
@@ -17,26 +17,29 @@ async def lego(event):
        pass
  await event.reply('Processing! Pls Weit...')
  text = event.pattern_match.group(1)
- img = Image.open('./Luna/resources/IMG_20210316_204512_022.jpg').convert('RGBA')
- draw = ImageDraw.Draw(img)
- image_widthz, image_heightz = img.size
- pointsize = 500
- fillcolor = "white"
- shadowcolor = "black"
- font = ImageFont.truetype('./Luna/resources/Vermin Vibes V.otf', 160)
- w, h = draw.textsize(text, font=font)
- h += int(h*0.21)
- image_width, image_height = img.size
- draw.text(((image_widthz-w)/2, (image_heightz-h)/2), text, font=font, fill=(240,248,255,128))
- x = (image_widthz-w)/2
- y= ((image_heightz-h)/2+6)
- draw.text((x, y), text, font=fnt, fill=(250,250,210,255), stroke_width=6, stroke_fill="black")
- fname2 = "lel.png"
- img.save(fname2, "png")
- await tbot.send_file(event.chat_id, fname2, caption="Made By Anie")
+# get an image
+ base = Image.open('./Luna/resources/IMG_20210316_204512_022.jpg').convert('RGBA')
+
+# make a blank image for the text, initialized to transparent text color
+ txt = Image.new('RGBA', base.size, (255,255,255,0))
+
+# get a font
+ fnt = ImageFont.truetype('./Luna/resources/Vermin Vibes V.otf', 160)
+# get a drawing context
+ d = ImageDraw.Draw(txt)
+
+# draw text, half opacity
+ d.text((50,550), text, font=fnt, fill=(240,248,255,128))
+# draw text, full opacity
+ d.text((40,500), text, font=fnt, fill=(250,250,210,255), stroke_width=6, stroke_fill="black")
+
+ out = Image.alpha_composite(base, txt)
+ fname = 'lel.png'
+ out.save(fname, "png")
+ await tbot.send_file(event.chat_id, fname, caption="Made By Anie")
  if os.path.exists(fname2):
-         os.remove(fname2)
- 
+            os.remove(fname2)
+
 file_help = os.path.basename(__file__)
 file_help = file_help.replace(".py", "")
 file_helpo = file_help.replace("_", " ")
