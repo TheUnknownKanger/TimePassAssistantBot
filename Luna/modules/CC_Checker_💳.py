f198b6e5939a5528b4479a0ddf17ee52eeeaf25f
Amarnath C <amarnathcharichilil@gmail.com>
2021-03-18 13:50:00 +0530

    Update CC_Checker_💳.py

--- Luna/modules/CC_Checker_💳.py
+++ Luna/modules/CC_Checker_💳.py
@@ -93,6 +93,16 @@ async def alive(event):
           await asyncio.sleep(18)
           response = await bot_conv.get_response()
           await event.reply(response)
+@register(pattern="^/au (.*)")
+async def alive(event):
+      ok = event.pattern_match.group(1)
+      async with ubot.conversation("@Carol5_bot") as bot_conv:
+          await bot_conv.send_message(f"/au {ok}")
+          k = await event.reply("**Wait for Result.**")
+          await asyncio.sleep(18)
+          response = await bot_conv.get_response()
+          await event.reply(response)
+
 
 @register(pattern="^/bin (.*)")
 async def alive(event):
