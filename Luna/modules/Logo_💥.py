b0763cedc8ac09d148eded431b386d1b2d096c05
Amarnath C <amarnathcharichilil@gmail.com>
2021-03-18 20:19:19 +0530

    Update Logo_💥.py

--- Luna/modules/Logo_💥.py
+++ Luna/modules/Logo_💥.py
@@ -29,7 +29,7 @@ async def lego(event):
     pointsize = 500
     fillcolor = "gold"
     shadowcolor = "blue"
-    font = ImageFont.truetype("./Luna/resources/Chopsic.otf", 300)
+    font = ImageFont.truetype("./Luna/resources/Chopsic.otf", 160)
     w, h = draw.textsize(text, font=font)
     h += int(h*0.21)
     image_width, image_height = img.size
