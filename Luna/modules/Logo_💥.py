a72314874872c6a26cf2df4282d8ead4d1f78e90
Amarnath C <amarnathcharichilil@gmail.com>
2021-03-18 10:52:02 +0530

    Update Logo_💥.py

--- Luna/modules/Logo_💥.py
+++ Luna/modules/Logo_💥.py
@@ -7,32 +7,45 @@ from PIL import Image, ImageDraw, ImageFont
 
 @register(pattern="^/logo ?(.*)")
 async def lego(event):
+ quew = event.pattern_match.group(1)
+ if "|" in quew:
+        text, bg, font = quew.split("|")
+     text = text.strip()
+     bg = bg.strip()
+     fnt = font.strip()
+ else:
+    text = quew
  if event.sender_id == OWNER_ID:
      pass
  else:
      if event.is_group:
-       await event.reply('Currently This Module Only Works in my PM!\n[Pls Click Here](tg://user?id=1624337697)')
+       await event.reply('Currently This Module Only Works in my [PM](tg://user?id=1624337697)')
        return
      else:
        pass
  await event.reply('Processing! Pls Weit...')
- text = event.pattern_match.group(1)
+ 
 # get an image
- base = Image.open('./Luna/resources/IMG_20210316_204512_022.jpg').convert('RGBA')
+ if bg == None or bg == '2':
+       base = Image.open('./Luna/resources/photo_2021-03-18_10-37-51.jpg').convert('RGBA')
+ elif if bg == '1':
+       base = Image.open('./Luna/resources/IMG_20210316_204512_022.jpg').convert('RGBA')
 
 # make a blank image for the text, initialized to transparent text color
  txt = Image.new('RGBA', base.size, (255,255,255,0))
 
 # get a font
- ft = ImageFont.truetype('./Luna/resources/Distort Me.otf', 160)
- fnt = ImageFont.truetype('./Luna/resources/Vermin Vibes V.otf', 160)
+ if fnt == None or fnt == '2':
+      font = ImageFont.truetype('./Luna/resources/Distort Me.otf', 160)
+ elif fnt == '1'
+      font = ImageFont.truetype('./Luna/resources/Vermin Vibes V.otf', 160)
 # get a drawing context
  d = ImageDraw.Draw(txt)
 
 # draw text, half opacity
- d.text((50,550), text, font=fnt, fill=(240,248,255,128))
+ d.text((50,550), text, font=font, fill=(240,248,255,128))
 # draw text, full opacity
- d.text((40,500), text, font=fnt, fill=(250,250,210,255), stroke_width=6, stroke_fill="black")
+ d.text((40,500), text, font=font, fill=(250,250,210,255), stroke_width=6, stroke_fill="black")
 
  out = Image.alpha_composite(base, txt)
  fname = 'lel.png'
