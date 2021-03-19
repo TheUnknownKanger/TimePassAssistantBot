d99ee4d99d5adf285e2aa1c1cc050265976916d5
Amarnath C <amarnathcharichilil@gmail.com>
2021-03-19 21:55:07 +0530

    Update Federation_🔗.py

--- Luna/modules/Federation_🔗.py
+++ Luna/modules/Federation_🔗.py
@@ -1,19 +1,3 @@
-#    MissJuliaRobot (A Telegram Bot Project)
-#    Copyright (C) 2019-2021 Julia (https://t.me/MissJulia_Robot)
-
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published by
-#    the Free Software Foundation, in version 3 of the License.
-
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see < https://www.gnu.org/licenses/agpl-3.0.en.html/ >.
-
-
 import csv
 import json
 import os
@@ -724,7 +708,7 @@ async def _(event):
             # Will send to current chat
             await tbot.send_message(
                 chat,
-                "<b>New Federation Ban</b>"
+                "<b>New FedBan</b>"
                 "\n<b>Federation:</b> {}"
                 "\n<b>Federation Admin:</b> {}"
                 "\n<b>User:</b> {}"
