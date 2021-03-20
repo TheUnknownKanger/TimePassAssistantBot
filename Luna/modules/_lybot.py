dd1728e6eae421409ed46cfc81de47b88b966210
Amarnath C <amarnathcharichilil@gmail.com>
2021-03-20 20:10:17 +0530

    Update _lybot.py

--- Luna/modules/_lybot.py
+++ Luna/modules/_lybot.py
@@ -24,11 +24,11 @@ async def inline_query(client, bot, query):
 @register(pattern="^/music (.*)")
 async def lybot(event):
    k = event.pattern_match.group(1)
-   await (await inline_query(ubot, "@lybot", k))[0].click("@aniegrpbot")
    async with tbot.conversation("@roseloverxm") as bot_conv:
       response = bot_conv.wait_event(
                 events.NewMessage(incoming=True, from_users="@RoseLoverxm")
             )
+      await (await inline_query(ubot, "@lybot", k))[0].click("@aniegrpbot")
       response = await response
       await asyncio.sleep(1)
       await response.forward_to(event.chat_id)
