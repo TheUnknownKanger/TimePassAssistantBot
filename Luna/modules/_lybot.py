3fce451f34d8cdaa70f700319ebc8c87901b8612
Amarnath C <amarnathcharichilil@gmail.com>
2021-03-20 22:30:56 +0530

    Update _lybot.py

--- Luna/modules/_lybot.py
+++ Luna/modules/_lybot.py
@@ -53,7 +53,6 @@ async def lybot(event):
             )
       await (await inline_query(ubot, "@HowGayBot", k))[0].click("@aniegrpbot")
       response = await response
-      await asyncio.sleep(1)
       await tbot.send_message(event.chat_id, response.text)
 
 
