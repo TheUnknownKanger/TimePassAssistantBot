3e31fd8a61129b6be7c240b78ac44cacbcfcd456
Amarnath C <amarnathcharichilil@gmail.com>
2021-03-21 20:55:50 +0530

    Create Song_🎵.py

---
+++ Luna/modules/Song_🎵.py
@@ -0,0 +1,30 @@
+from Luna import CMD_HELP, OWNER_ID
+from Luna.events import register
+import os
+
+k = 'News Py Files In Another Repo'
+@register(pattern="^/teg ?(.*)")
+async def sleepybot(time):
+    if time.fwd_from:
+        return
+    if time.sender_id == OWNER_ID:
+        pass
+    else:
+        return
+    await time.reply("Song Py In App 2")
+
+file_help = os.path.basename(__file__)
+file_help = file_help.replace(".py", "")
+file_helpo = file_help.replace("_", " ")
+
+__help__ = """
+ - /song: Downloads and sends the song from YouTube
+ - /music: The faster way to get songs.
+"""
+
+CMD_HELP.update({
+    file_helpo: [
+        file_helpo,
+        __help__
+    ]
+})
