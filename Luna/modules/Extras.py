f7477899abe6e8e8d7dfd0c0a32ba81278562d0f
Amarnath C <amarnathcharichilil@gmail.com>
2021-03-22 14:06:04 +0530

    Update Extras.py

--- Luna/modules/Extras.py
+++ Luna/modules/Extras.py
@@ -40,8 +40,7 @@ Example syntax: `/crypto inr btc`
  - /google: Performs A Google Search With given Query
  - /img: Search Google for images and returns them\nFor greater no. of results specify lim, For eg: `/img hello lim=10`
  - /reverse: Does a reverse image search of the media which it was replied to.
- - @AniegrpBot yt <query>: Inline YouTube Search. **N:**use ; to specify no. Of results
-"""
+ - @AniegrpBot yt <query>: Inline YouTube Search. **N:**use ; to specify no. Of results.
 """
 
 CMD_HELP.update({file_helpo: [file_helpo, __help__]})
