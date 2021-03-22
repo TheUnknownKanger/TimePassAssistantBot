da03e72e492bfcfdb4f4221f8e681d58a5b9a8ae
Amarnath C <amarnathcharichilil@gmail.com>
2021-03-22 17:41:16 +0530

    Create Warnings.py

---
+++ Luna/modules/Warnings.py
@@ -0,0 +1,19 @@
+from Luna.events import bot
+
+
+
+
+
+file_help = os.path.basename(__file__)
+file_help = file_help.replace(".py", "")
+file_helpo = file_help.replace("_", " ")
+
+__help__ = """
+ - /warn <userid> | <reason> or /warn <reason> in reply to a message: warn a user
+ - /removelastwarn: remove the last warn that a user has received
+ - /getwarns: list the warns that a user has received
+ - /resetwarns: reset all warns that a user has received
+ - /setwarnmode <kick/ban/mute>: set the warn mode for the chat
+"""
+
+CMD_HELP.update({file_helpo: [file_helpo, __help__]})
