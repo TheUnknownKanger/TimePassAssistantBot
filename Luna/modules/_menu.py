2dc23c1e2c9272e3da35e2718393272c7eb141fd
Amarnath C <amarnathcharichilil@gmail.com>
2021-03-22 11:34:24 +0530

    Update _menu.py

--- Luna/modules/_menu.py
+++ Luna/modules/_menu.py
@@ -122,7 +122,7 @@ async def help_menu(event):
 
 @tbot.on(events.CallbackQuery(pattern=r"fun_help"))
 async def help_menu(event):
-    buttons = paginate_help(event, 0, FUN_LIST, "helpme")
+    buttons = page(event, 0, FUN_LIST, "helpme")
     await event.edit(pm_caption, buttons=buttons)
 
 
@@ -164,6 +164,41 @@ async def on_plug_in_callback_query_handler(event):
     except BaseException:
         pass
 
+@tbot.on(events.callbackquery.CallbackQuery(data=re.compile(rb"helpme_lel\((.+?)\)")))
+async def on_plug_in_callback_query_handler(event):
+    current_page_number = int(event.data_match.group(1).decode("UTF-8"))
+    buttons = paginate_help(event, current_page_number + 1, FUN_LIST, "helpme")
+    await event.edit(buttons=buttons)
+
+@tbot.on(events.callbackquery.CallbackQuery(data=re.compile(rb"helpme_ull\((.+?)\)")))
+async def on_plug_in_callback_query_handler(event):
+    current_page_number = int(event.data_match.group(1).decode("UTF-8"))
+    buttons = paginate_help(event, current_page_number - 1, FUN_LIST, "helpme")
+    await event.edit(buttons=buttons)
+
+@tbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"dat_plugin_(.*)")))
+async def on_plug_in_callback_query_handler(event):
+    plugin_name = event.data_match.group(1).decode("UTF-8")
+    help_string = ""
+    # By @RoseLoverX
+
+    for i in FUN_LIST[plugin_name]:
+        plugin = plugin_name.replace("_", " ")
+        emoji = plugin_name.split("_")[0]
+        output = str(CMD_HELP[plugin][1])
+        help_string = f"Here is the help for **{emoji}**:\n" + output
+
+    if help_string is None:
+        pass  # stuck on click
+    else:
+        reply_pop_up_alert = help_string
+    try:
+        await event.edit(
+            reply_pop_up_alert, buttons=[
+                [Button.inline("Back", data="go_back")]]
+        )
+    except BaseException:
+        pass
 
 @tbot.on(events.CallbackQuery(pattern=r"go_back"))
 async def go_back(event):
@@ -179,7 +214,7 @@ def get_page(id):
 
 
 def paginate_help(event, page_number, loaded_plugins, prefix):
-    number_of_rows = 9
+    number_of_rows = 15
     number_of_cols = 4
 
     to_check = get_page(id=event.sender_id)
@@ -224,6 +259,57 @@ def paginate_help(event, page_number, loaded_plugins, prefix):
                 custom.Button.inline("◀️", data="reopen_again"),
                 custom.Button.inline(
                     "➡", data="{}_next({})".format(prefix, modulo_page)
+                ),
+            )
+        ]
+    return pairs
+
+def page(event, page_number, loaded_plugins, prefix):
+    number_of_rows = 8
+    number_of_cols = 3
+
+    to_check = get_page(id=event.sender_id)
+
+    if not to_check:
+        pagenumber.insert_one({"id": event.sender_id, "page": page_number})
+
+    else:
+        pagenumber.update_one(
+            {
+                "_id": to_check["_id"],
+                "id": to_check["id"],
+                "page": to_check["page"],
+            },
+            {"$set": {"page": page_number}},
+        )
+
+    helpable_plugins = []
+    for p in loaded_plugins:
+        if not p.startswith("_"):
+            helpable_plugins.append(p)
+    helpable_plugins = sorted(helpable_plugins)
+    modules = [
+        custom.Button.inline(
+            "{}".format(x.replace("_", " ")), data="dat_plugin_{}".format(x)
+        )
+        for x in helpable_plugins
+    ]
+    pairs = list(zip(modules[::number_of_cols], modules[1::number_of_cols], modules[2::number_of_cols]))
+    if len(modules) % number_of_cols == 1:
+        pairs.append((modules[-1],))
+    max_num_pages = ceil(len(pairs) / number_of_rows)
+    modulo_page = page_number % max_num_pages
+    if len(pairs) > number_of_rows:
+        pairs = pairs[
+            modulo_page * number_of_rows: number_of_rows * (modulo_page + 1)
+        ] + [
+            (
+                custom.Button.inline(
+                    "⏮️", data="{}_lel({})".format(prefix, modulo_page)
+                ),
+                custom.Button.inline("◀️", data="reopen_again"),
+                custom.Button.inline(
+                    "➡", data="{}_ull({})".format(prefix, modulo_page)
                 ),
             )
         ]
