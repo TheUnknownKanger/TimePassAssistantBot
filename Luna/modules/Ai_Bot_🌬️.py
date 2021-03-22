9e77d03b3f2cb1a1befd21b67a2ab96abfef3284
Amarnath C <amarnathcharichilil@gmail.com>
2021-03-22 11:16:49 +0530

    Update Ai_Bot_🌬️.py

--- Luna/modules/Ai_Bot_🌬️.py
+++ Luna/modules/Ai_Bot_🌬️.py
@@ -1,7 +1,7 @@
 import requests
 url = "https://iamai.p.rapidapi.com/ask"
 from Luna import tbot, OWNER_ID, CMD_HELP
-from Luna.events import register
+from Luna.events import bot as register
 from telethon import events
 from telethon import types
 from telethon.tl import functions
